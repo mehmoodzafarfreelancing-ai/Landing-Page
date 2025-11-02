1260bc429892aa554de39ce9f31af335f3db33d9
mehmoodzafarfreelancing-ai <mehmoodzafarfreelancing@gmail.com>
2025-11-03 02:04:51 +0500

    Hamburger For Design

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
-const hamMenu = document.querySelector('.ham-menu');
-const offScreenMenu = document.querySelector('.off-screen-menu');
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('open');  // Changed 'active' to 'open' to match CSS
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+document.addEventListener('click', () => {
+    navLinks.classList.toggle("active");
 })
